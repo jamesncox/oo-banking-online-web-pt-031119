8e316391e3c5cc10b30a15efd9ee18847620c91a
James Cox <jamesncox@yahoo.com>
2019-04-09 16:24:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,9 +25,9 @@ class Transfer
     else
       @sender.deposit(@amount*-1)
       @receiver.deposit(@amount)
-      binding.pry
       status = "complete"
     end
+    binding.pry
   end
   
 end

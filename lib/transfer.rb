dca14048b7543a2a164e75ac5c8166f4047b22c6
James Cox <jamesncox@yahoo.com>
2019-04-09 15:37:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
-  attr_accessor :transfer, :sender, :receiver
+  attr_accessor :transfer, :sender, :receiver, :status
   attr_reader
   
   def initialize(transfer)
     @transfer = transfer
     @sender = sender
     @receiver = receiver
+    @status = "pending"
   end
   
 end
